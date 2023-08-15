becd7feb406688f45b8ea4119120bc5363996020
sunny kumar <jaissunny158@gmail.com>
2023-08-15 13:21:41 +0530

    Arrange

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
+
 const screen=document.querySelector('input');
 const buttons=document.querySelectorAll('button');
+
 for(let btn of buttons){
+
     btn.addEventListener('click',(e)=>{
+
         const text=e.target.innerText;
+
         if(text==='C'){
             screen.value="";
         }
+
         else if(text==='x'){
             screen.value+='*';
         }
+
         else if(text==='='){
             try{
                 screen.value=eval(screen.value);
@@ -18,6 +25,9 @@ for(let btn of buttons){
                 screen.value="Invalid Operation"
             }
         }
-        else screen.value+=text;
+
+        else {
+            screen.value+=text;
+        }
     })
 }
