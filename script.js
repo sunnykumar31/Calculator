0ee32a5b6eb88f7e45dc0bfa59a64f345d03ab6b
sunny kumar <jaissunny158@gmail.com>
2023-08-15 16:36:57 +0530

    History

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 const screen=document.querySelector('input');
 const buttons=document.querySelectorAll('button');
+const list=document.querySelector('ol');
 
 for(let btn of buttons){
 
@@ -17,15 +18,22 @@ for(let btn of buttons){
         }
 
         else if(text==='='){
+
             try{
+                const li=document.createElement('li');
+                let t=screen.value;
+                t+='=';
                 screen.value=eval(screen.value);
+                t+=screen.value;
+                li.innerText=t;
+                list.append(li);
+
                 
             }
             catch{
                 screen.value="Invalid Operation"
             }
         }
-
         else {
             screen.value+=text;
         }
