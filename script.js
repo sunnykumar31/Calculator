3b6d8b1db0f9eb802361f9642acc60bcca1c0218
sunny kumar <jaissunny158@gmail.com>
2023-08-16 12:11:55 +0530

    UndoButtonAdded

--- script.js
+++ script.js
@@ -2,18 +2,20 @@
 const screen=document.querySelector('input');
 const buttons=document.querySelectorAll('button');
 const list=document.querySelector('ol');
-
+let prev;
+let ar=[];
 for(let btn of buttons){
 
     btn.addEventListener('click',(e)=>{
 
         const text=e.target.innerText;
-
         if(text==='C'){
+            prev=ar.length;
             screen.value="";
         }
 
         else if(text==='x'){
+            prev=ar.length;
             screen.value+='*';
         }
 
@@ -22,17 +24,31 @@ for(let btn of buttons){
             try{
                 const li=document.createElement('li');
                 let t=screen.value;
+                ar.push(t);
+                prev=ar.length;
                 t+='=';
                 screen.value=eval(screen.value);
                 t+=screen.value;
                 li.innerText=t;
                 list.append(li);
+                
 
                 
             }
             catch{
                 screen.value="Invalid Operation"
             }
+        }
+        else if(text==='Undo'){
+            if(prev==0){
+                alert("No Previous Operation is Available Now!!")
+            }
+            else{
+                screen.value=ar[prev-1];
+                prev--;
+            }
+            // screen.value=prev;
+            
         }
         else {
             screen.value+=text;
